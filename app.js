484fa27f25c13fdb89674215bffaa4392b9ad684
peterbuk8 <peterbuk8@gmail.com>
2016-08-14 18:04:10 -0600

    Fixed navbar, auto scroll and formatting

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ angular.module('app', []).controller('MainController', ['$scope', function($scop
     $scope.content = {'title': 'foo'};
 
     $scope.month = 0;
+
+    $scope.goTop = function() {
+        //window.scrollTo(0, 0);
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    }
 }]);
